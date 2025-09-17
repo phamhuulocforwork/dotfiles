badb6f85aa6e62f2ccd700487b3c5a66ccc21990
Pham Huu Loc <phamhuulocforwork@gmail.com>
2025-09-17 14:52:46 +0700

    chore: add copy-configs.sh script to streamline configuration file copying for various applications

---
+++ copy-configs.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cp -r -l $HOME/.config/alacritty/ ./.config
+cp -r -l $HOME/.config/dunst/ ./.config
+cp -r -l $HOME/.config/hypr/ ./.config
+cp -r -l $HOME/.config/kitty/ ./.config
+cp -r -l $HOME/.config/rofi/ ./.config
+cp -r -l $HOME/.config/waybar/ ./.config
+cp -r -l $HOME/.config/wlogout/ ./.config
+cp -r -l $HOME/.zshrc ./zsh
+cp -r -l $HOME/.p10k.zsh ./zsh
