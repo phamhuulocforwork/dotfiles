5c07de1b3e453304394d4493a4bd626586f740e8
Pham Huu Loc <phamhuulocforwork@gmail.com>
2025-09-17 21:20:18 +0700

    fix: update setup.sh to simplify yay installation by changing directory structure for cloning

--- setup.sh
+++ setup.sh
@@ -85,8 +85,8 @@ _installYay() {
     _installPackages "base-devel"
     SCRIPT=$(realpath "$0")
     temp_path=$(dirname "$SCRIPT")
-    git clone https://aur.archlinux.org/yay.git $download_folder/yay
-    cd $download_folder/yay
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
     makepkg -si
     cd $temp_path
     echo ":: yay has been installed successfully."
