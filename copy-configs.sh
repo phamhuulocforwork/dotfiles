9b62ffa83e37d1088f3313929920094bcfad45ff
Pham Huu Loc <phamhuulocforwork@gmail.com>
2025-09-17 15:25:20 +0700

    chore: update copy-configs.sh to copy configuration files from the repository to the user's HOME directory

--- copy-configs.sh
+++ copy-configs.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
-# Tạo thư mục .config nếu chưa có
-mkdir -p ./.config
+# Tạo thư mục .config trong HOME nếu chưa có
+mkdir -p $HOME/.config
 
-# Copy các config files, đè lên file cũ nếu có
-cp -r $HOME/.config/alacritty/ ./.config/
-cp -r $HOME/.config/dunst/ ./.config/
-cp -r $HOME/.config/hypr/ ./.config/
-cp -r $HOME/.config/kitty/ ./.config/
-cp -r $HOME/.config/rofi/ ./.config/
-cp -r $HOME/.config/waybar/ ./.config/
-cp -r $HOME/.config/wlogout/ ./.config/
-cp $HOME/.zshrc ./
-cp $HOME/.zsh_aliases ./
-cp $HOME/.p10k.zsh ./
-cp $HOME/.gitconfig ./
+# Copy các config files từ repo vào HOME, đè lên file cũ nếu có
+cp -r ./.config/alacritty/ $HOME/.config/
+cp -r ./.config/dunst/ $HOME/.config/
+cp -r ./.config/hypr/ $HOME/.config/
+cp -r ./.config/kitty/ $HOME/.config/
+cp -r ./.config/rofi/ $HOME/.config/
+cp -r ./.config/waybar/ $HOME/.config/
+cp -r ./.config/wlogout/ $HOME/.config/
+cp ./.zshrc $HOME/
+cp ./.zsh_aliases $HOME/
+cp ./.p10k.zsh $HOME/
